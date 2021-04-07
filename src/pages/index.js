447c0df8c99e408bb6b5663ca3a5bcb6cb97dd00
Stackbit Code Editor <projects@stackbit.com>
2021-04-07 06:55:50 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
