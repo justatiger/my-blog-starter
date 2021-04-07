01d55af5fa32468996be98da1388c03baa300a2c
Stackbit Code Editor <projects@stackbit.com>
2021-04-07 06:54:42 +0000

    index.js: changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -82,6 +82,11 @@ export const pageQuery = graphql`
           date(formatString: "MMMM DD, YYYY")
           title
           description
+          featuredImage {
++            childImageSharp {
++              gatsbyImageData(width: 800)
++            }
++          }
         }
       }
     }
